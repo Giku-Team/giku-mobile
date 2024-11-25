f5805136b9d782c69d57614adde6925da859cdb4
Alfred Sahala Nainggolan <alfred.nainggolan00@gmail.com>
2024-11-25 22:42:31 +0700

    feat: add new color palette and update the app themes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,18 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("com.google.devtools.ksp")
 }
+
+val properties = Properties().apply {
+    load(file("local.properties").inputStream())
+}
+
+val baseUrl: String = properties.getProperty("BASE_URL") ?: "default_url"
+val loginUrl: String = properties.getProperty("LOGIN_URL") ?: "default_url"
+val registerUrl: String = properties.getProperty("REGISTER_URL") ?: "default_url"
 
 android {
     namespace = "com.mobile.giku"
@@ -16,15 +26,25 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = false
+            buildConfigField("String", "BASE_URL", "\"${baseUrl}\"")
+            buildConfigField("String", "LOGIN_URL", "\"${loginUrl}\"")
+            buildConfigField("String", "REGISTER_URL", "\"${registerUrl}\"")
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            buildConfigField("String", "BASE_URL", "\"${baseUrl}\"")
+            buildConfigField("String", "LOGIN_URL", "\"${loginUrl}\"")
+            buildConfigField("String", "REGISTER_URL", "\"${registerUrl}\"")
         }
     }
     compileOptions {
@@ -36,6 +56,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
