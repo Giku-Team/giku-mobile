3f2e683658beaea8a226e45bd76b3f1c595c6e8d
Alfred Sahala Nainggolan <alfred.nainggolan00@gmail.com>
2024-11-28 16:49:52 +0700

    feat: slice login page layout

--- app/src/main/java/com/mobile/giku/view/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/mobile/giku/view/ui/auth/AuthActivity.kt
@@ -1,21 +1,24 @@
 package com.mobile.giku.view.ui.auth
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.mobile.giku.R
 import com.mobile.giku.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
 
     private lateinit var  binding: ActivityAuthBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+    val navHostFragment = supportFragmentManager.findFragmentById(R.id.auth_nav_host_fragment) as NavHostFragment
+    navController = navHostFragment.navController
 
     }
 }
