6f22baadb4882e25f912363a8d61e6136cc01507
Alfred Sahala Nainggolan <alfred.nainggolan00@gmail.com>
2024-12-05 17:58:58 +0700

    feat(auth): implement forgot password feature with email verification flow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -131,6 +131,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.coil)
+    implementation(libs.barcode.scanning)
 }
 
 tasks.register("printVersionName") {
