6fb113d8c0d01436e81f007417300f4e6d154f06
Alfred Sahala Nainggolan <alfred.nainggolan00@gmail.com>
2024-12-10 00:38:53 +0700

    fix(main): update navigation between bottom navigation bar using navigation component

--- app/src/main/java/com/mobile/giku/view/ui/main/MainActivity.kt
+++ app/src/main/java/com/mobile/giku/view/ui/main/MainActivity.kt
@@ -2,61 +2,50 @@ package com.mobile.giku.view.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.widget.ViewPager2
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.mobile.giku.R
 import com.mobile.giku.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewPager: ViewPager2
-    private lateinit var fragmentAdapter: MainFragmentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setupViewPagerAndBottomNavigation()
+        setupBottomNavigation()
     }
 
-    private fun setupViewPagerAndBottomNavigation() {
-        fragmentAdapter = MainFragmentAdapter(this)
-        viewPager = binding.viewPager
-        viewPager.adapter = fragmentAdapter
+    private fun setupBottomNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.setupWithNavController(navController)
 
         binding.bottomNavigation.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.nav_home -> viewPager.currentItem = 0
-                R.id.nav_camera -> viewPager.currentItem = 1
-                R.id.nav_profile -> viewPager.currentItem = 2
-            }
-            true
-        }
-
-        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                when (position) {
-                    0 -> binding.bottomNavigation.selectedItemId = R.id.nav_home
-                    1 -> binding.bottomNavigation.selectedItemId = R.id.nav_camera
-                    2 -> binding.bottomNavigation.selectedItemId = R.id.nav_profile
+                R.id.nav_home -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
                 }
+                R.id.nav_camera -> {
+                    navController.navigate(R.id.cameraFragment)
+                    true
+                }
+                R.id.nav_profile -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+                else -> false
             }
-        })
+        }
     }
 
-    class MainFragmentAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
-        override fun getItemCount(): Int = 3
-
-        override fun createFragment(position: Int): Fragment {
-            return when (position) {
-                0 -> HomeFragment()
-                1 -> CameraFragment()
-                2 -> ProfileFragment()
-                else -> throw IllegalArgumentException("Invalid position")
-            }
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.main_nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
