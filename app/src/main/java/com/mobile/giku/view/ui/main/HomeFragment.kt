6a94bc34abb2b2913d701b66e2713e4f7d6b71ba
Alfred Sahala Nainggolan <alfred.nainggolan00@gmail.com>
2024-12-10 00:18:10 +0700

    feat(calendar): add horizontal calendar and configure it

--- app/src/main/java/com/mobile/giku/view/ui/main/HomeFragment.kt
+++ app/src/main/java/com/mobile/giku/view/ui/main/HomeFragment.kt
@@ -1,30 +1,53 @@
 package com.mobile.giku.view.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mobile.giku.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mobile.giku.databinding.FragmentHomeBinding
+import com.mobile.giku.view.adapter.CalendarAdapter
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.Calendar
+import java.util.Locale
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    private val today = LocalDate.now()
+    private val weekDates = mutableListOf<LocalDate>().apply {
+        repeat(7) { add(today.plusDays(it.toLong())) }
+    }
+    private val calendar: Calendar = Calendar.getInstance()
+    private val dateFormat = SimpleDateFormat("MMMM, yyyy", Locale.getDefault())
+    private val currentDate: String = dateFormat.format(calendar.time)
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupCalendarRecyclerView()
+    }
 
+    private fun setupCalendarRecyclerView() {
+        val adapter = CalendarAdapter(weekDates)
+        binding.mtrlCalendarDaySelectorFrame.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            this.adapter = adapter
+        }
+        binding.textMonthAndYearCalendar.text = currentDate
     }
 
     override fun onDestroyView() {
