4cda6260b5d00bb96b2318a322d9c9f606227122
Alfred Sahala Nainggolan <alfred.nainggolan00@gmail.com>
2024-12-10 20:22:26 +0700

    feat(nutrient): setup NutrientApiService and implement NutrientRepository

---
+++ app/src/main/java/com/mobile/giku/model/remote/nutrient/NutrientApiService.kt
@@ -0,0 +1,16 @@
+package com.mobile.giku.model.remote.nutrient
+
+import com.mobile.giku.BuildConfig
+import okhttp3.MultipartBody
+import retrofit2.Response
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface NutrientApiService {
+    @Multipart
+    @POST(BuildConfig.NUTRITION_PREDICTION_URL)
+    suspend fun nutritionPrediction(
+        @Part image: MultipartBody.Part
+    ): Response<NutritionPredictionResponse>
+}
