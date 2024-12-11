9554f4ce2401efce87047126966a83db8edabdd0
Alfred Sahala Nainggolan <alfred.nainggolan00@gmail.com>
2024-12-11 21:17:00 +0700

    feat(nutrient): slicing analysis details page design layout

---
+++ app/src/main/java/com/mobile/giku/view/ui/analysis/AnalysisDetailsFragment.kt
@@ -0,0 +1,33 @@
+package com.mobile.giku.view.ui.analysis
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mobile.giku.databinding.FragmentAnalysisDetailsBinding
+
+class AnalysisDetailsFragment : Fragment() {
+
+    private var _binding: FragmentAnalysisDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAnalysisDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
